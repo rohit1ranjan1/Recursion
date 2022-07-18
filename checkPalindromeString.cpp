3e1da86a228e2811074d29f73774290280c23b3d
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-18 17:29:19 +0530

    Create checkPalindromeString.cpp

---
+++ checkPalindromeString.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool reverseArray(char a[], int i, int n){
+    if(i>=n)
+    return true;
+    
+    if(a[i]!=a[n]){
+        cout<<"Not palindrome!";
+        return false;
+    }
+    
+    bool ifP=reverseArray(a,i+1,n-1);
+    return ifP;
+}
+int main() {
+    char arr[4]={'a','b','c','d'};
+    if(reverseArray(arr,0,3))
+    cout<<"Palindrome!";
+    return 0;
+}
+
+/*
+Not palindrome!
+*/
