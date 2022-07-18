bf3061adf901e3e335cc355be1ab395302c34eed
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-18 16:54:11 +0530

    Update reverseArray.cpp

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,20 +1,23 @@
-
 #include <iostream>
 using namespace std;
-int* reverseArray(int *a, int i, int n){
+void reverseArray(int a[], int i, int n){
     if(i==n)
-    return a;
+    return;
     
     int swap=a[n];
     a[n]=a[i];
     a[i]=swap;
-    a=reverseArray(a,i+1,n-1);
-    return a;
+    reverseArray(a,i+1,n-1);
+    return;
 }
 int main() {
     int arr[5]={1,2,3,4,5};
-    arr=reverseArray(&arr[i],0,4);
+    reverseArray(arr,0,4);
     for(int i=0;i<5;i++)
     cout<<arr[i]<<" ";
     return 0;
 }
+
+/*
+5 4 3 2 1
+*/
